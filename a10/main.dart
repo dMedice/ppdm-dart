ac24596563a2e217f30ed561b11314df18fc9564
dMedice <133895157+dMedice@users.noreply.github.com>
2024-03-27 13:59:47 -0300

    Add files via upload

---
+++ a10/main.dart
@@ -0,0 +1,9 @@
+import 'classes.dart';
+
+void main() {
+  Pessoa pessoa1 = new Pessoa("Emerson Martins Magalhães", "14023185812", 12);
+
+  print(pessoa1.cpf);
+  print(pessoa1.nome);
+  print(pessoa1.idade);
+}
